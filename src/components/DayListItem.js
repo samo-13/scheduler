1076936c4cbbad6c441391b10ca507d394c11d4f
samo-13 <sarah-moss@live.com>
2022-07-27 17:49:01 +0200

    Add DayListItem event handler

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -1,10 +1,15 @@
-import React from "react";
+import React from "react";  
 
 export default function DayListItem(props) {
+  const setDay = function() {
+    console.log('Hi from setDay function');
+  }
+
+
   return (
-    <li>
-      <h2 className="text--regular">Day Name</h2> 
-      <h3 className="text--light">X spots remaining</h3>
+    <li onClick={() => props.setDay(props.name)}>
+      <h2 className="text--regular" onClick={setDay}>{props.name}</h2> 
+      <h3 className="text--light">{props.spots} Left Remaining</h3>
     </li>
   );
 }
