23bc97ea1c54e85b25518d369564e4aaf67c693d
samo-13 <sarah-moss@live.com>
2022-08-10 09:44:56 -0400

    Add initial Confirm component

---
+++ src/components/Appointment/Confirm.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "components/Appointment/styles.scss";
+import "components/Button";
+// import Header from "components/Appointment/Header.js";
+// import classNames from "classnames";
+
+// The <Confirm> component should accept the following props:
+
+// --- message:String eg. "Delete the appointment?"
+// --- onConfirm:Function to be called when the user clicks the Confirm button
+// --- onCancel:Function to be called when the user clicks the Cancel button
+
+export default function Confirm(props) {
+  return (
+    <main className="appointment__card appointment__card--confirm">
+      <h1 className="text--semi-bold">Delete the appointment?</h1>
+      <section className="appointment__actions">
+        <Button danger>Cancel</Button>
+        <Button danger>Confirm</Button>
+      </section>
+    </main>
+  )
+}
