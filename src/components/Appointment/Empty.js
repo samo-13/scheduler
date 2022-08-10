6bf8c0660fae1d7a2c61808ad07869db8a20caba
samo-13 <sarah-moss@live.com>
2022-08-09 23:15:34 -0400

    Create Empty component

---
+++ src/components/Appointment/Empty.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "components/Appointment/styles.scss";
+// import DayListItem from "components/DayListItem.js";
+// import classNames from "classnames";
+
+export default function Empty(props) {
+  return (
+    <main className="appointment__add">
+      <img
+        className="appointment__add-button"
+        src="images/add.png"
+        alt="Add"
+      />
+    </main>
+  )
+}
