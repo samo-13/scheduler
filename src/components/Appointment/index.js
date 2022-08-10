4af0b1ed7035e87882fffb97c082db248d9c30b5
samo-13 <sarah-moss@live.com>
2022-08-09 22:39:56 -0400

    Setup Appointment component

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+// import DayListItem from "components/DayListItem.js";
+// import classNames from "classnames";
+
+export default function Appointment(props) {
+  return (
+    <article className="appointment"></article>
+  )
+}
