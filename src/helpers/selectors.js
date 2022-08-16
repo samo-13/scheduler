f9387d0209afb0bd94a05b1a3d80569de168d29a
samo-13 <sarah-moss@live.com>
2022-08-16 15:16:52 -0400

    Begin fixing selectors

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,47 +1,47 @@
 
 // -----------------------------------------------------------------------------------------------
 
-// const state = {
-//   days: [
-//     {
-//       id: 1,
-//       name: "Monday",
-//       appointments: [1, 2, 3]
-//     },
-//     {
-//       id: 2,
-//       name: "Tuesday",
-//       appointments: [4, 5]
-//     }
-//   ],
-//   appointments: {
-//     "1": { id: 1, time: "12pm", interview: null },
-//     "2": { id: 2, time: "1pm", interview: null },
-//     "3": {
-//       id: 3,
-//       time: "2pm",
-//       interview: { student: "Archie Cohen", interviewer: 2 }
-//     },
-//     "4": { id: 4, time: "3pm", interview: null },
-//     "5": {
-//       id: 5,
-//       time: "4pm",
-//       interview: { student: "Chad Takahashi", interviewer: 2 }
-//     }
-//   },
-//   interviewers: {
-//     "1": {  
-//       "id": 1,
-//       "name": "Sylvia Palmer",
-//       "avatar": "https://i.imgur.com/LpaY82x.png"
-//     },
-//     "2": {
-//       id: 2,
-//       name: "Tori Malcolm",
-//       avatar: "https://i.imgur.com/Nmx0Qxo.png"
-//     }
-//   }
-// };
+const state = {
+  days: [
+    {
+      id: 1,
+      name: "Monday",
+      appointments: [1, 2, 3]
+    },
+    {
+      id: 2,
+      name: "Tuesday",
+      appointments: [4, 5]
+    }
+  ],
+  appointments: {
+    "1": { id: 1, time: "12pm", interview: null },
+    "2": { id: 2, time: "1pm", interview: null },
+    "3": {
+      id: 3,
+      time: "2pm",
+      interview: { student: "Archie Cohen", interviewer: 2 }
+    },
+    "4": { id: 4, time: "3pm", interview: null },
+    "5": {
+      id: 5,
+      time: "4pm",
+      interview: { student: "Chad Takahashi", interviewer: 2 }
+    }
+  },
+  interviewers: {
+    "1": {  
+      "id": 1,
+      "name": "Sylvia Palmer",
+      "avatar": "https://i.imgur.com/LpaY82x.png"
+    },
+    "2": {
+      id: 2,
+      name: "Tori Malcolm",
+      avatar: "https://i.imgur.com/Nmx0Qxo.png"
+    }
+  }
+};
 
 // -----------------------------------------------------------------------------------------------
 
@@ -51,44 +51,49 @@
 // -- Access that specific days appointment array.
 // -- Iiterate through it, comparing where it's id matches the id of states.appointments and return that value.
 // -- Do validation.
-// -- If there are no appointments on the given day, our days data will be empty. 
-// -- According to our tests, in a case like this, we should return an empty array.
 
-export function getAppointmentsForDay(state, day) {
+const getAppointmentsForDay = function(state, day) {
   // console.log('this.state:', this.state)
-  // console.log('day:', state.days)
-  for (const d of state.days) {
-    // console.log('d:', d);
-    // const d = days[i];
-    // console.log('d.name:', d.name)
-    if (d.name === day) {
-      // console.log('d.appointments:', d.appointments)
-      // console.log('appointments:', state.appointments)
-      const appointmentsArray = d.appointments;
-      // console.log('appointmentsArray:', appointmentsArray);
-      const results = appointmentsArray.map((a) => getAppointmentArray(a, state));
-      // console.log('results:', results.flat())
-      return results.flat();
-      // return appointmentsArray;
+  console.log('state.days.length:', state.days.length);
+  console.log('state.days:', state.days);
+  console.log('state:', state);
+  console.log('day:', day);
+  console.log('state.days.name:', state.days[0].name)
+
+  for (let i = 0; i < state.days.length; i ++) {
+    if (state.days[i].name === day) {
+
+      console.log (`Found! ${day}`);
+      console.log(`Appointments: ${state.days[i].appointments}`)
     }
+    // console.log('state.days.name:', state.days[i].name)
+    // return state.days[i].name
   }
-  return [];
+
+  if (state.days.length === 0) { // If there are no appointments on the given day, our days data will be empty -- return an empty array.
+    return [];
+  }
+  return 'hi!'
 }
 
-const getAppointmentArray = function(id, state) {
-  const appointmentResults = [];
+getAppointmentsForDay(state, "Monday")
+getAppointmentsForDay(state, "Tuesday")
+getAppointmentsForDay(state, "Wednesday")
+
+// const getAppointmentArray = function(id, state) {
+//   const appointmentResults = [];
   
-  for (const a of Object.values(state.appointments)) {
-    // console.log(a.id)
-    // console.log(id)
-    if (a.id === id) {
-      appointmentResults.push(a)
-    }
-    // console.log('appointment results:', appointmentResults)
-  }
-  return appointmentResults;
-  // return appointments.id
-}
+//   for (const a of Object.values(state.appointments)) {
+//     // console.log(a.id)
+//     // console.log(id)
+//     if (a.id === id) {
+//       appointmentResults.push(a)
+//     }
+//     // console.log('appointment results:', appointmentResults)
+//   }
+//   return appointmentResults;
+//   // return appointments.id
+// }
 
 // getAppointmentsForDay(state, 'Tuesday')
 
@@ -105,19 +110,19 @@ const getAppointmentArray = function(id, state) {
 //   }
 // }
 
-export function getInterview(state, interview){
-    // console.log('interview:', interview)
-    // console.log('state.appointments["3"].interview:', state.appointments["3"].interview)
-    if (interview) {
-      const newObj = {}
-      const interviewerID = interview.interviewer
-      newObj.student = interview.student
-      newObj.interviewer = state.interviewers[interviewerID]
-      // console.log('newObj:', newObj);
-      return newObj;
-    }
-  // console.log('No appt booked!')
-  return null;
-}
+// export function getInterview(state, interview){
+//     // console.log('interview:', interview)
+//     // console.log('state.appointments["3"].interview:', state.appointments["3"].interview)
+//     if (interview) {
+//       const newObj = {}
+//       const interviewerID = interview.interviewer
+//       newObj.student = interview.student
+//       newObj.interviewer = state.interviewers[interviewerID]
+//       // console.log('newObj:', newObj);
+//       return newObj;
+//     }
+//   // console.log('No appt booked!')
+//   return null;
+// }
 
 // getInterview(state,'2')
