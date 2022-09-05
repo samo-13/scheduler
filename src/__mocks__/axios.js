66e7479160d260f4b8af9805e28fe3b6ad61b479
samo-13 <sarah-moss@live.com>
2022-09-05 15:49:08 -0400

    Create mocking modules for Application

---
+++ src/__mocks__/axios.js
@@ -0,0 +1,89 @@
+const fixtures = {
+  days: [
+    {
+      id: 1,
+      name: "Monday",
+      appointments: [1, 2],
+      interviewers: [1, 2],
+      spots: 1
+    },
+    {
+      id: 2,
+      name: "Tuesday",
+      appointments: [3, 4],
+      interviewers: [3, 4],
+      spots: 1
+    }
+  ],
+  appointments: {
+    "1": { id: 1, time: "12pm", interview: null },
+    "2": {
+      id: 2,
+      time: "1pm",
+      interview: { student: "Archie Cohen", interviewer: 2 }
+    },
+    "3": {
+      id: 3,
+      time: "2pm",
+      interview: { student: "Leopold Silvers", interviewer: 4 }
+    },
+    "4": { id: 4, time: "3pm", interview: null }
+  },
+  interviewers: {
+    "1": {
+      id: 1,
+      name: "Sylvia Palmer",
+      avatar: "https://i.imgur.com/LpaY82x.png"
+    },
+    "2": {
+      id: 2,
+      name: "Tori Malcolm",
+      avatar: "https://i.imgur.com/Nmx0Qxo.png"
+    },
+    "3": {
+      id: 3,
+      name: "Mildred Nazir",
+      avatar: "https://i.imgur.com/T2WwVfS.png"
+    },
+    "4": {
+      id: 4,
+      name: "Cohana Roy",
+      avatar: "https://i.imgur.com/FK8V841.jpg"
+    }
+  }
+};
+
+// --------------------------------------------------------------------------------
+// fixture: can be used to describe reusable static data that is imported or embedded into a test file. 
+// make sure we have accurate data that matches the schema from the server.
+// --------------------------------------------------------------------------------
+
+export default { // export our fake axios library
+  defaults: { baseURL: "" },
+  get: jest.fn(url => { // mocks the get function using jest.fn() to create the mock
+    if (url === "http://localhost:8001/api/days") {
+      return Promise.resolve({ // return a resolved Promise to match the interface used by axios -- skip the whole sequence of HTTP operations and resolve a hardcoded version of the response data.
+        status: 200,
+        statusText: "OK",
+        data: fixtures.days
+      });
+    }
+
+    if (url === "http://localhost:8001/api/appointments") {
+      return Promise.resolve({ // return a resolved Promise to match the interface used by axios -- skip the whole sequence of HTTP operations and resolve a hardcoded version of the response data.
+        status: 200,
+        statusText: "OK",
+        data: fixtures.appointments
+      });
+    }
+
+    if (url === "http://localhost:8001/api/interviewers") {
+      return Promise.resolve({ // return a resolved Promise to match the interface used by axios -- skip the whole sequence of HTTP operations and resolve a hardcoded version of the response data.
+        status: 200,
+        statusText: "OK",
+        data: fixtures.interviewers
+      });
+    }
+  })
+}
+
--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,14 +1,55 @@
 import React from "react";
 
-import { render, cleanup } from "@testing-library/react";
-import { renderHook } from '@testing-library/react'
+import { render, cleanup, waitForElement, fireEvent } from "@testing-library/react";
+// import { renderHook } from '@testing-library/react'
 
 import Application from "components/Application";
 
+
+
 afterEach(cleanup);
 
-// change test.skip back to it to run test
 
-test.skip("renders without crashing", () => {
-  render(<Application />);
+// We will mock the functions we use from the axios library.
+// We will write a test to confirm that the scheduler can load data.
+// We will write an asynchronous test that waits for a component to update before proceeding.
+// We will use containers to find specific DOM nodes.
+// We will chain promises to handle asynchronous testing.
+// We will override mock implementations for specific tests.
+// We will use setup and teardown functions provided by Jest to perform common tasks.
+
+// The tests we are writing now are mostly integration tests. 
+// We are testing the components and the custom hooks that they use. 
+// In some cases, it will make more sense to isolate certain logic. 
+// It will allow us to import the specific concern and test it in isolation.
+
+// Testing is an art, not a science. These examples are intended to offer a limited introduction to a certain style of testing.
+
+// Throughout these examples it is possible that coverage numbers and source code will differ between projects. 
+// There will be a limited amount of detail in the instructions. 
+// Use documentation, previous activities and existing tests to research the queries and matchers for some of these more challenging tests.
+
+// change test.skip back to it to run test
+// test.skip("renders without crashing", () => {
+//   render(<Application />);
+// });
+
+// --------------------------------------------------------------------------------
+// We can make our test asynchronous by returning a Promise.
+// --------------------------------------------------------------------------------
+
+// it("defaults to Monday and changes the schedule when a new day is selected", () => {
+//   const { getByText } = render(<Application />);
+//   return waitForElement(() => getByText("Monday")); // Uses waitForElement to wait until we are able to get a DOM element with the text "Monday" -- // The waitForElement function returns a promise that resolves when the callback returns a truthy value and rejects after a time out when it cannot find the specified text. 
+// // When we return a Promise from the test function, the Jest framework knows that the test isn't complete until the promise chain has resolved or rejected.
+// // The argument we pass to waitForElement is a function that returns a DOM node. In this case, it is looking for something based on the text "Monday".
+// });
+
+it("defaults to Monday and changes the schedule when a new day is selected", () => {
+  const { getByText } = render(<Application />);
+
+  return waitForElement(() => getByText("Monday")).then(() => {
+    fireEvent.click(getByText("Tuesday"));
+    expect(getByText("Leopold Silvers")).toBeInTheDocument();
+  });
 });
