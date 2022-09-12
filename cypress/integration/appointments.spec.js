ea43a3acfdff529cdb452b0e9412dbf74c13f04a
samo-13 <sarah-moss@live.com>
2022-09-12 19:21:32 -0400

    Add click save to appointment test

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -7,5 +7,6 @@
         .click();
       cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones");
       cy.get("[alt='Sylvia Palmer']").click();
+      cy.contains("Save").click();
   });
 })
