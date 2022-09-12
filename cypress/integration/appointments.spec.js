10ee8b25974d8cd9042c71b503486f3782fbfbe4
samo-13 <sarah-moss@live.com>
2022-09-12 19:35:10 -0400

    Add click edit button to appointment test

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -1,15 +1,30 @@
-  describe("Appointments", () => {
-    it("should book an interview", () => {
-      cy.request("GET", "/api/debug/reset")
-      cy.visit("/");
-      cy.contains("Monday");
-      cy.get("[alt=Add]")
-        .first()
-        .click();
-      cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones");
-      cy.get("[alt='Sylvia Palmer']").click();
-      cy.contains("Save").click();
-      cy.contains(".appointment__card--show", "Lydia Miller-Jones");
-      cy.contains(".appointment__card--show", "Sylvia Palmer");
+describe("Appointments", () => {
+  beforeEach(() => {
+    cy.request("GET", "/api/debug/reset");
+  
+    cy.visit("/");
+  
+    cy.contains("Monday");
   });
-})
+ 
+  it("should book an interview", () => {
+    cy.get("[alt=Add]")
+      .first()
+      .click();
+  
+    cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones");
+    cy.get('[alt="Sylvia Palmer"]').click();
+  
+    cy.contains("Save").click();
+  
+    cy.contains(".appointment__card--show", "Lydia Miller-Jones");
+    cy.contains(".appointment__card--show", "Sylvia Palmer");
+  });
+
+  it("should edit an interview", () => {
+    cy.get("[alt=Edit]") // https://docs.cypress.io/api/commands/hover#Invoke 
+    .first()
+    .click
+    ({ force: true })
+  })
+ });
